aa539a47e70420db90080fa0652fc850c94242a0
sabbir <ahmedsabbirsm77@gmail.com>
2024-05-16 12:13:06 +0600

    task 2 working

--- oop.php
+++ oop.php
@@ -1,54 +1,71 @@
 <?php
 class Book {
  // TODO: Add properties as Private
+ private $title;
+ private $availableCopies;
  
  public function __construct($title, $availableCopies) {
      // TODO: Initialize properties
+     $this->title = $title;
+     $this->availableCopies = $availableCopies;
      }
 
 
 // TODO: Add getTitle method
-
-
- 
-
-
-
- // TODO: Add getAvailableCopies method
- 
-
-
-
- // TODO: Add borrowBook method
- 
-
-
-
- // TODO: Add returnBook method
- 
+public function getTitle() {
+    return $this->title;
 }
 
+ // TODO: Add getAvailableCopies method
+ public function getAvailableCopies() {
+     return $this->availableCopies;
+ }
+ // TODO: Add borrowBook method
+ public function borrowBook() {
+     if ($this->availableCopies > 0) {
+         $this->availableCopies--;
+         return true;
+     } else {
+        echo "No available copies of '$this->title'";
+         return false;
+     }
+ }
+ // TODO: Add returnBook method
+ public function returnBook() {
+     $this->availableCopies++;
+     echo "Book '{$this->title}' returned successfully";
+ }
+}
 
 class Member {
  // TODO: Add properties as Private
-
-
+ private $name;
 
  public function __construct($name) {
       // TODO: Initialize properties
+      $this->name = $name;
      }
 
-
-
  // TODO: Add getName method
+ public function getName() {
+     return $this->name;
+ }
  
  // TODO: Add borrowBook method
+ public function borrowBook($book) {
+    if ($book->borrowBook()) {
+        echo "{$this->name} borrowed '{$book->getTitle()}'\n";
+    }
+}
  
 
 
 
  // TODO: Add returnBook method
  
+ public function returnBook($book) {
+    $book->returnBook();
+}
 }
 
 
@@ -63,19 +80,26 @@ class Member {
 //Book 1 - Name: The Great Gatsby, Available Copies: 5.
 //Book 2 - Name: To Kill a Mockingbird, Available Copies: 3.
 
-
+$book1 = new Book("The Great Gatsby", 5);
+$book2 = new Book("To Kill a Mockingbird", 3);
 
 // TODO: Create 2 members with the following properties
 //Member 1 - Name: John Doe
 //Member 2 - Name: Jane Smith
+$member1 = new Member("John Doe");
+$member2 = new Member("Jane Smith");
 
 
 
 // TODO: Apply Borrow book method to each member
+$member1->borrowBook($book1);
+$member2->borrowBook($book2);
 
 
 
 // TODO: Print Available Copies with their names:
+echo "Available Copies of '{$book1->getTitle()}': {$book1->getAvailableCopies()}\n";
+echo "Available Copies of '{$book2->getTitle()}': {$book2->getAvailableCopies()}\n";
 
 
 ?>

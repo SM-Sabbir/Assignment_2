f9942059aad03f8cbbc63b01071ec3c02912ae88
sabbir <ahmedsabbirsm77@gmail.com>
2024-05-16 11:11:59 +0600

    Task 1 working

--- string.php
+++ string.php
@@ -2,4 +2,23 @@
 
 $strings = ["Hello", "World", "PHP", "Programming"];
 
+foreach ($strings as $string) {
+    $originalString = $string;
+    $vowelCount = countVowels($string);
+    $reversedString = strrev($string);
+    
+    echo "Original String: $originalString, Vowel Count: $vowelCount, Reversed String: $reversedString\n";
+}
 
+function countVowels($string) {
+    $vowels = ['a', 'e', 'i', 'o', 'u'];
+    $count = 0;
+    foreach (str_split(strtolower($string)) as $char) {
+        if (in_array($char, $vowels)) {
+            $count++;
+        }
+    }
+    return $count;
+}
+
+?>

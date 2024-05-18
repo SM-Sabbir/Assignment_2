92a0f434026cc972d47237cb1c56f4a9dad618d4
sabbir <ahmedsabbirsm77@gmail.com>
2024-05-18 08:59:27 +0600

    Reformatted

--- oop.php
+++ oop.php
@@ -98,6 +98,3 @@ $member2->borrowBook($book2);
 // TODO: Print Available Copies with their names:
 echo "Available Copies of '{$book1->getTitle()}': {$book1->getAvailableCopies()}\n";
 echo "Available Copies of '{$book2->getTitle()}': {$book2->getAvailableCopies()}\n";
-
-
-?>
--- string.php
+++ string.php
@@ -21,4 +21,3 @@ function countVowels($string) {
     return $count;
 }
 
-?>
